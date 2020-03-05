1db9e83d090dd9c92612f4a749ccc61da699dfae
Esteban Mallen <esteban.mallen@users.noreply.github.com>
2020-03-05 17:36:32 -0300

    Added javadocs

--- src/main/java/com/stvmallen/minesweeper/service/CellService.java
+++ src/main/java/com/stvmallen/minesweeper/service/CellService.java
@@ -8,11 +8,37 @@ import com.stvmallen.minesweeper.model.GameBean;
 import com.stvmallen.minesweeper.model.NewGameRequest;
 
 public interface CellService {
+	/**
+	 * Reveals a given cell, adding to its data the amount of adjacent mines. If none,
+	 * adjacent cells will be revealed too.
+	 *
+	 * @param gameBean the game bean
+	 * @param cellId   the id of the cell to reveal
+	 * @throws MineExplodedException if the revealed cell contains a mine, this exception will be thrown
+	 */
 	void revealCell(GameBean gameBean, Long cellId) throws MineExplodedException;
 
+	/**
+	 * Marks a cell as potentially mined
+	 *
+	 * @param gameBean the game bean
+	 * @param cellId   the cell id to mark
+	 */
 	void markCell(GameBean gameBean, Long cellId);
 
+	/**
+	 * Flags a cell as a mine
+	 *
+	 * @param gameBean the game bean
+	 * @param cellId   the cell id to flag
+	 */
 	void flagCell(GameBean gameBean, Long cellId);
 
+	/**
+	 * Creates the cell matrix with the given parameters, adding mines in random positions
+	 *
+	 * @param request the cell matrix parameteres
+	 * @return the list of cells created
+	 */
 	List<CellBean> createCells(NewGameRequest request);
 }
