9ae97c43d72a46cadc272b6abab71474005bb1bb
Esteban Mallen <esteban.mallen@users.noreply.github.com>
2020-03-05 00:50:29 -0300

    Changed repos to CrudRepository

--- src/main/java/com/stvmallen/minesweeper/repository/CellRepository.java
+++ src/main/java/com/stvmallen/minesweeper/repository/CellRepository.java
@@ -1,10 +1,11 @@
 package com.stvmallen.minesweeper.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.stvmallen.minesweeper.entity.Cell;
 
 @Repository
-public interface CellRepository extends JpaRepository<Cell, Long> {
+public interface CellRepository extends CrudRepository<Cell, Long> {
+
 }
--- src/main/java/com/stvmallen/minesweeper/repository/GameRepository.java
+++ src/main/java/com/stvmallen/minesweeper/repository/GameRepository.java
@@ -1,10 +1,10 @@
 package com.stvmallen.minesweeper.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.stvmallen.minesweeper.entity.Game;
 
 @Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository extends CrudRepository<Game, Long> {
 }
