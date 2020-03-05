19bbf686e5c4e77c05a89c27c381d16ae9222836
Esteban Mallen <esteban.mallen@users.noreply.github.com>
2020-03-05 11:12:56 -0300

    Added default timestamps to entities

--- src/main/java/com/stvmallen/minesweeper/entity/TimestampedEntity.java
+++ src/main/java/com/stvmallen/minesweeper/entity/TimestampedEntity.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PreUpdate;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +14,12 @@ import lombok.Setter;
 @Setter
 abstract class TimestampedEntity extends AbstractEntity {
 	@Column(name = "created_on", nullable = false)
-	private Instant createdOn;
+	private Instant createdOn = Instant.now();
 	@Column(name = "last_modified", nullable = false)
-	private Instant lastModified;
+	private Instant lastModified = Instant.now();
+
+	@PreUpdate
+	public void updateLastModified() {
+		lastModified = Instant.now();
+	}
 }
