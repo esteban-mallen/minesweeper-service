25927ac9ca515baa1d6dda368f16add2112ed1fe
Esteban Mallen <esteban.mallen@users.noreply.github.com>
2020-03-04 23:23:01 -0300

    Created SpringBoot application

---
+++ src/test/java/com/stvmallen/minesweeper/MinesweeperApplicationTest.java
@@ -0,0 +1,12 @@
+package com.stvmallen.minesweeper;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+public class MinesweeperApplicationTest {
+	@Test
+	public void testApplicationContext() {
+		//no-op: if test passes mean the application context loaded correctly
+	}
+}
